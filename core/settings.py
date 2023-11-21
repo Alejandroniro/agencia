e663db7580fe5a627562bd2c2e41abb1f969b5a2
Alejandro Niño <102247526+Alejandroniro@users.noreply.github.com>
2023-11-21 18:11:33 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -170,7 +170,7 @@ if not DEBUG:
     CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEPLOY')
      # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
