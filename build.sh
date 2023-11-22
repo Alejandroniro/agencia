01e8649d50c7d3d08e6590c69e4e87adfe53d66b
alejandroniro <ajnr1999@gmail.com>
2023-11-22 00:18:03 -0400

    revert

--- build.sh
+++ build.sh
@@ -11,4 +11,3 @@ npm run build
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
-python3 manage.py shell -c "from core.wsgi import create_superuser; create_superuser()"
--- core/wsgi.py
+++ core/wsgi.py
@@ -8,29 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
 
-
-def create_superuser():
-    users = User.objects.all()
-    if not users:
-        User.objects.create_superuser(
-            username="root",
-            email="root@root.com",
-            password="claveroot",
-            is_active=True,
-            is_staff=True,
-        )
-
-
-# Llama a la función para crear el superusuario al iniciar el servidor.
-create_superuser()
-
-
-from django.core.management import call_command
-call_command('runserver', '0.0.0.0:8000')
